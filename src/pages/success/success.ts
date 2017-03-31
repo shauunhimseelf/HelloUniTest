00b3ca78f93fcabcf536aa091f8a0c73a7644f7e
Shaun McWhinnie <shaun@howzitgaun.com>
2017-03-31 09:33:46 +0200

    Last files added

---
+++ src/pages/success/success.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-success',
+  templateUrl: 'success.html'
+})
+export class SuccessPage {
+
+  constructor() {
+
+  }
+
+}
