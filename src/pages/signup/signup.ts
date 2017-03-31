793c9815d8dce3cbecec280e4f1dcb2c43f32168
Shaun McWhinnie <shaun@howzitgaun.com>
2017-03-31 17:13:59 +0200

    Added FormBuilder functions to signup form

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,13 +1,28 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'page-signup',
   templateUrl: 'signup.html',
 })
 export class SignupPage {
+  private signup : FormGroup;
 
-  constructor() {
+  constructor( private formBuilder: FormBuilder ) {
+    this.signup = this.formBuilder.group({
+      name: ['', Validators.required, ],
+      dob: ['', Validators.required],
+      gender: ['', Validators.required],
+      nationality: ['', Validators.required],
+      current_school: ['', Validators.required],
+      current_major: ['', Validators.required],
+      current_gpa: ['', Validators.required]
+    })
+  }
 
+  logSignup() {
+    console.log(this.signup.value);
   }
 
   @Output()
