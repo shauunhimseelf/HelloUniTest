fb5250ddff0729aba669991845ae764ca9eafec5
Shaun McWhinnie <shaun@howzitgaun.com>
2017-03-31 18:23:18 +0200

    Edited submit functions to merge object

--- src/pages/slider/slider.ts
+++ src/pages/slider/slider.ts
@@ -8,6 +8,7 @@ import { Slides } from 'ionic-angular';
   selector: 'page-slider',
   templateUrl: 'slider.html'
 })
+
 export class SliderPage {
 
   private userDetails : Object;
@@ -15,6 +16,17 @@ export class SliderPage {
   @ViewChild(Slides) slides: Slides;
 
   constructor() {
+    this.userDetails = {
+      name: "",
+      dob: "",
+      gender: "",
+      nationality: "",
+      current_school: "",
+      current_major: "",
+      current_gpa: "",
+      label: [],
+      Lifestyle: []
+    }
 
   }
 
@@ -23,16 +35,22 @@ export class SliderPage {
   }
 
   signupSubmitted( evt ) {
-    console.log(evt);
     this.slides.slideTo(1, 500);
+    this.updateUserDetails(evt);
   }
 
   aboutSubmitted( evt ) {
     this.slides.slideTo(2, 500);
+    this.updateUserDetails(evt);
+    console.log(this.userDetails);
   }
 
   resetSubmitted( evt ) {
     this.slides.slideTo(0, 100);
+  }
+
+  updateUserDetails( obj ){
+    this.userDetails = Object.assign( obj, this.userDetails );
   }
 
 }
